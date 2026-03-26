bbd2cf1dcaa4ce89bcf7f9b990ffd73184b0ce36
Thecapstone <105176170+Thecapstone@users.noreply.github.com>
2026-03-26 13:13:28 +0100

    update env variables

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5w-o!7wk65!)r#in*v24)cw)y=zhh$baw#sfa1ioo&=5t$m7sn'
+SECRET_KEY = os.environ.get("SECRET_KEY", "bananA123")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
