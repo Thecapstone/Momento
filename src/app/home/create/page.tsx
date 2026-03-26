efb3b97785e3bbe496228e21578827412aa77824
jomoartz <johnjohananmogbolu@gmail.com>
2026-03-26 23:27:14 +0100

    feat: create capsule and capsule detail page

---
+++ src/app/home/create/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { CreateCapsuleForm } from "@/components/capsule/CreateCapsuleForm";
+
+const CreateCapsule = () => {
+  return (
+    <div className="bg-capsule_amber">
+      <CreateCapsuleForm />
+    </div>
+  );
+};
+
+export default CreateCapsule;
