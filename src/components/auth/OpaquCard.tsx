ada8315a69de9607cc9b9cfbb0f734d1223a3692
jomoartz <johnjohananmogbolu@gmail.com>
2026-03-26 11:19:09 +0100

    feat: opaquCard

--- src/components/auth/OpaquCard.tsx
+++ src/components/auth/OpaquCard.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 const OpaquCard = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex h-screen flex-col justify-end md:justify-center md:items-center">
-      <div className="flex flex-col justify-center gap-10 rounded-t-3xl md:rounded-3xl bg-white/40 h-[70vh] md:h-auto md:w-fit">
+      <div className="flex flex-col justify-center gap-10 rounded-t-3xl md:rounded-3xl bg-white/40 h-fit md:h-auto md:w-fit">
         {children}
       </div>
     </div>
