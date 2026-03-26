0af9d5931c0d1a6b5d4ec6b4da3f7e8e33c32756
jomoartz <johnjohananmogbolu@gmail.com>
2026-03-26 14:30:22 +0100

    feat: setup landing page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import { Landing } from "@/components/home/LandingSection";
+
+
+function page() {
+  return (
+    <div className="flex flex-col w-full h-auto">
+      <Landing />
+    </div>
+  );
+}
+
+export default page;
