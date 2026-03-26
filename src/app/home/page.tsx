c76a0262a1a969ed68865701a34d2bd63f2c7720
jomoartz <johnjohananmogbolu@gmail.com>
2026-03-26 23:26:31 +0100

    feat: added capusle page to homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,12 @@
+import CapsulesPage from "@/components/capsule/CapsuleSection";
 import { Landing } from "@/components/home/LandingSection";
 
 
 function page() {
   return (
-    <div className="flex flex-col w-full h-auto">
+    <div className="flex flex-col w-full h-fit">
       <Landing />
+      <CapsulesPage/>
     </div>
   );
 }
