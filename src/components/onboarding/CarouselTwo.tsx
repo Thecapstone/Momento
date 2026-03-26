a7140c529740572a2dcc9a64ec9c27c149942fe4
jomoartz <johnjohananmogbolu@gmail.com>
2026-03-26 01:10:29 +0100

    feat: Carousel two

---
+++ src/components/onboarding/CarouselTwo.tsx
@@ -0,0 +1,26 @@
+import { CarouseAnimationStyles } from "@/app/lib/onboardingUtils";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const {animate, initial, transition} = CarouseAnimationStyles
+
+export const CarouselTwo = () => (
+  <div className="flex flex-col items-center">
+      <motion.p className="hidden md:block text-gray-600 max-w-md text-lg lg:text-xl font-medium"
+      animate={animate}
+      initial={initial}
+      transition={transition}
+      >
+      Secure your work. Preserve your legacy one project at a time.
+      </motion.p>
+
+
+    <Image
+      src="/svg/lock_n_key.svg"
+      width={300}
+      height={300}
+      alt="company logo"
+    />
+
+  </div>
+);
